cdae9694251b3bcb7a1c3c4b4552957f0e26a2ef
kuldeepsinh2005 <kuldeepsinh2005kad@gmail.com>
2026-03-26 10:41:56 +0530

    deployment docker issue

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -8,7 +8,7 @@ import { useKey } from "./KeyContext";
 
 // 1. Create Axios instance globally
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL ,
   withCredentials: true, 
 });
 
